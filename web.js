59ad609af2764eeaf328ba41dbba3004b55e149e
fnovella <fnovella@gmail.com>
2013-07-05 17:08:58 +0200

    Updated web.js

--- web.js
+++ web.js
@@ -1,12 +1,15 @@
 var express = require('express');
+var fs      = require('fs');
 
-var app = express.createServer(express.logger());
+var app     = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-});
+var buffer  = new buffer(fs.readFileSync('index.html'));
+
+app.get('/',
+        function(req, res) { res.send(buffer.toString()); }
+       );
 
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+app.listen(port, function()
+                 { console.log("Listening on " + port); }
+          );
